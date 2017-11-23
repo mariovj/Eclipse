7f27e8186d26f6da37125a1a53f621722439301a
Hola soy Mario, encantado <m.valera1_1@hotmail.com>
2017-11-23 13:30:23 +0100

    He añadido suma

--- HolaMundo/src/HelloWorld.java
+++ HolaMundo/src/HelloWorld.java
@@ -3,7 +3,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
-
+		double x = 8.99;
+		double y = 4.8776;
+		double z;
+		z = x+y;
+		System.out.println(z);
 		
 		// TODO Auto-generated method stub
 
